d1d30bab086d551b1ea57aa65ea8fc5bd80beb11
Joshua Reibert <joshua.reibert@uni-weimar.de>
2018-10-11 10:52:33 +0200

    Format

--- src/vec2.hpp
+++ src/vec2.hpp
@@ -8,33 +8,33 @@ struct Vec2 {
   Vec2(double x, double y);
   ~Vec2() = default;
 
-  Vec2(const Vec2 &) = default;
-  Vec2(Vec2 &&other) noexcept;
+  Vec2(const Vec2&) = default;
+  Vec2(Vec2&& other) noexcept;
 
-  Vec2 &operator=(const Vec2 &) = default;
-  Vec2 &operator=(Vec2 &&other) noexcept;
+  Vec2& operator=(const Vec2&) = default;
+  Vec2& operator=(Vec2&& other) noexcept;
 
-  Vec2 operator+(const Vec2 &other);
-  Vec2 operator-(const Vec2 &other);
-  Vec2 &operator+=(const Vec2 &other);
-  Vec2 &operator-=(const Vec2 &other);
+  Vec2 operator+(const Vec2& other);
+  Vec2 operator-(const Vec2& other);
+  Vec2& operator+=(const Vec2& other);
+  Vec2& operator-=(const Vec2& other);
 
   Vec2 operator+(double s);
   Vec2 operator-(double s);
   Vec2 operator*(double s);
   Vec2 operator/(double s);
 
-  Vec2 &operator+=(double s);
-  Vec2 &operator-=(double s);
-  Vec2 &operator*=(double s);
-  Vec2 &operator/=(double s);
+  Vec2& operator+=(double s);
+  Vec2& operator-=(double s);
+  Vec2& operator*=(double s);
+  Vec2& operator/=(double s);
 
-  Vec2 &normalize();
-  double distance(const Vec2 &other) const;
+  Vec2& normalize();
+  double distance(const Vec2& other) const;
   double length() const;
 
-  static double dot(const Vec2 &lhs, const Vec2 &rhs);
-  static double cross(const Vec2 &lhs, const Vec2 &rhs);
+  static double dot(const Vec2& lhs, const Vec2& rhs);
+  static double cross(const Vec2& lhs, const Vec2& rhs);
 
   double x;
   double y;
