115d9391260193bffcbcf683a3c7179b1067b929
Joshua Reibert <joshua.reibert@uni-weimar.de>
2018-10-22 15:10:02 +0200

    Add methods for properties like center, distance...
    Store initial properties at the time the pinch gesture is created.

--- src/gestures/pinch.cpp
+++ src/gestures/pinch.cpp
@@ -1,15 +1,64 @@
 #include <gestures/pinch.hpp>
 
 Pinch::Pinch(const std::set<std::shared_ptr<TouchPoint>>& first_cluster,
-             const std::set<std::shared_ptr<TouchPoint>>& second_cluster) {
+             const std::set<std::shared_ptr<TouchPoint>>& second_cluster)
+    : m_first_cluster(first_cluster), m_second_cluster(second_cluster) {
   m_touch_points.insert(m_touch_points.end(), first_cluster.begin(),
                         first_cluster.end());
   m_touch_points.insert(m_touch_points.end(), second_cluster.begin(),
                         second_cluster.end());
+
+  m_start_center = center();
+  m_start_distance = distance();
+  m_start_direction = direction();
+
+  m_horizontal =
+      (::angle(m_start_direction, Vec2(1.0, 0.0)) <
+       DIRECTION_ANGLE_THRESHOLD) ||
+      (::angle(m_start_direction, Vec2(-1.0, 0.0)) < DIRECTION_ANGLE_THRESHOLD);
+  if (!m_horizontal) {
+    m_vertical = (::angle(m_start_direction, Vec2(0.0, 1.0)) <
+                  DIRECTION_ANGLE_THRESHOLD) ||
+                 (::angle(m_start_direction, Vec2(0.0, -1.0)) <
+                  DIRECTION_ANGLE_THRESHOLD);
+  }
 }
 
 std::ostream& Pinch::print(std::ostream& stream) const {
+  if (m_horizontal) {
+    stream << "Horizontal";
+  } else if (m_vertical) {
+    stream << "Vertical";
+  }
   stream << "Pinch <";
   print_touch_points(stream);
   return stream << ">";
+}
+
+Vec2 Pinch::first_center() const {
+  std::vector<Vec2> positions;
+  std::transform(m_first_cluster.begin(), m_first_cluster.end(),
+                 std::back_inserter(positions),
+                 [](auto& tp) { return tp->pos(); });
+  return centroid(positions);
+}
+
+Vec2 Pinch::second_center() const {
+  std::vector<Vec2> positions;
+  std::transform(m_second_cluster.begin(), m_second_cluster.end(),
+                 std::back_inserter(positions),
+                 [](auto& tp) { return tp->pos(); });
+  return centroid(positions);
+}
+
+Vec2 Pinch::center() const {
+  return centroid({first_center(), second_center()});
+}
+
+double Pinch::distance() const {
+  return ::distance(first_center(), second_center());
+}
+
+Vec2 Pinch::direction() const {
+  return second_center() - first_center();
 }
--- src/gestures/pinch.hpp
+++ src/gestures/pinch.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <chrono>
+#include <cmath>
 #include <iostream>
 #include <set>
 
 #include <gestures/gesture.hpp>
+#include <vec2.hpp>
 
 class Pinch : public Gesture {
  public:
@@ -12,4 +14,25 @@ class Pinch : public Gesture {
         const std::set<std::shared_ptr<TouchPoint>>& second_cluster);
 
   std::ostream& print(std::ostream& stream) const override;
+
+  Vec2 first_center() const;
+  Vec2 second_center() const;
+  Vec2 center() const;
+  double distance() const;
+  Vec2 direction() const;
+  bool horizontal() const;
+  bool vertical() const;
+
+protected:
+  const double DIRECTION_ANGLE_THRESHOLD = 0.2 * M_PI;
+
+protected:
+  std::set<std::shared_ptr<TouchPoint>> m_first_cluster;
+  std::set<std::shared_ptr<TouchPoint>> m_second_cluster;
+
+  Vec2 m_start_center;
+  double m_start_distance;
+  Vec2 m_start_direction;
+  bool m_horizontal = false;
+  bool m_vertical = false;
 };
